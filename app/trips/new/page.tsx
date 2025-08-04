7757f4c4ae61d131375ba1e90e9a93d1c9ad48a0
ValerieAttila09 <akunvalerie76@gmail.com>
2025-08-04 20:08:00 +0700

    committed

---
+++ app/trips/new/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+
+export default function NewTrip() {
+
+
+
+  return (
+    <div className="max-w-lg mx-auto mt-10">
+      <Card>
+        <CardHeader>
+          New Trip
+        </CardHeader>
+        <CardContent>
+          <form action="" className="space-y-6">
+            <div>
+              <label htmlFor="" className="block text-sm outfit-medium text-neutral-700 mb-1">Title</label>
+              <input required type="text" name="title" id="title" placeholder="Japan trip..." className={cn("w-full border border-[#d7d7d7] px-3 py-2", "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500")} />
+            </div>
+            <div>
+              <label htmlFor="" className="block text-sm outfit-medium text-neutral-700 mb-1">Description</label>
+              <textarea required name="description" id="description" placeholder="Trip description..." className={cn("w-full border border-[#d7d7d7] px-3 py-2", "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500")} />
+            </div>
+            <div className="grid grid-cols-2 gap-x-4">
+              <div className="col-span-1">
+                <label htmlFor="" className="block text-sm outfit-medium text-neutral-700 mb-1">Start Date</label>
+                <input type="date" name="startDate" id="startDate" className={cn("w-full border border-[#d7d7d7] px-3 py-2", "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500")} />
+              </div>
+              <div className="col-span-1">
+                <label htmlFor="" className="block text-sm outfit-medium text-neutral-700 mb-1">End Date</label>
+                <input type="date" name="endDate" id="endDate" className={cn("w-full border border-[#d7d7d7] px-3 py-2", "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500")} />
+              </div>
+            </div>
+
+            <Button type="submit" className="w-full">
+              Create Trip
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -1,9 +1,11 @@
 import { auth } from "@/auth"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
-export default async function TripsPage(){
+export default async function TripsPage() {
 
   const session = await auth()
-  if(!session) {
+  if (!session) {
     return (
       <div className="p-4 flex items-center justify-center h-screen">
         <h1 className="text-4xl outfit-medium text-neutral-900">PLEASE SIGN IN.</h1>
@@ -15,7 +17,9 @@ export default async function TripsPage(){
     <div className="space-y-6 container mx-auto px-4 py-8">
       <div className="">
         <h1 className="">Dashboard</h1>
-        <Button>New Trip</Button>
+        <Link href={"/trips/new"}>
+          <Button>New Trip</Button>
+        </Link>
       </div>
     </div>
   )
