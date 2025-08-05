1b31f45b88c489f6f6fbde1bb8bb11e1a4dfab97
ValerieAttila09 <akunvalerie76@gmail.com>
2025-08-05 22:57:33 +0700

    committed

--- components/trip-detail.tsx
+++ components/trip-detail.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Trip } from "@/app/generated/prisma"
+import Image from "next/image";
 
 interface TripDetailClientProps {
   trip: Trip;
@@ -9,6 +10,12 @@ interface TripDetailClientProps {
 export default function TripDetailClient({ trip }: TripDetailClientProps) {
   return (
     <div className="container mx-auto px-4 py-8 space-y-8">
+      {trip.imageUrl && (
+        <div className="w-full h-72 md:h-96 overflow-hidden rounded-xl shadow-lg relative">
+          <Image src={trip.imageUrl} alt={trip.title} className="object-cover" fill priority />
+        </div>
+      )}
+
       
     </div>
   )
