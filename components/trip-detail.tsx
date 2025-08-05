64d68e357be8ee4e3173c7653ffec47e2d0fdf71
ValerieAttila09 <akunvalerie76@gmail.com>
2025-08-05 22:52:41 +0700

    committed

---
+++ components/trip-detail.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { Trip } from "@/app/generated/prisma"
+
+interface TripDetailClientProps {
+  trip: Trip;
+}
+
+export default function TripDetailClient({ trip }: TripDetailClientProps) {
+  return (
+    <div className="container mx-auto px-4 py-8 space-y-8">
+      
+    </div>
+  )
+}
